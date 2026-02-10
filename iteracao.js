3d19808802ddf6616b264b6e0bc78246899cff70
eduardomabaka <angelicawavanga@gmail.com>
2026-02-10 15:21:33 -0300

    fazendo a atualização

---
+++ iteracao.js
@@ -0,0 +1,28 @@
+var load = document.getElementById("loading")
+var corpo = document.getElementById("corpo")
+var footer = document.getElementById("f")
+function show(){
+    var mostrar = document.getElementById("show")
+    if (mostrar.style.display == "none"){
+        mostrar.style.display = "flex"
+    }else{
+        mostrar.style.display = "none"
+    }
+}
+function notshow(){
+    var mostrar = document.getElementById("show")
+    if (mostrar.style.display == "flex"){
+        mostrar.style.display = "none"
+    }
+}
+
+setTimeout(()=>{
+    corpo.style.display="none"
+    footer.style.display="none"
+    load.style.display="block"
+},0)
+setTimeout(()=>{
+    corpo.style.display="flex"
+    footer.style.display="none"
+    load.style.display="none"
+},3000)
